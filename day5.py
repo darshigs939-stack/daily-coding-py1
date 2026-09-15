2da85f9ffb90ed594974a8ba90c4d520a8d94f9b
darshigs939-stack <darshigs939@gmail.com>
2026-09-15 19:34:00 +0530

    Update day5.py

--- day5.py
+++ day5.py
@@ -1,1 +1,9 @@
+name= int(input('Enter your name'))
+n= int(input('Enter your age'))
+if n>=18:
+  print(f'{name},you can vote)
+elif n>=0:
+  print(f'{name},you cannot vote)
+else:
+  print('invalid age')
 
